a889372eb55a4f81c5b5fb7a0525a444b2b86934
abdulfarouqmusedde-cloud <abdulfarouqmusedde@gmail.com>
2026-06-14 11:18:06 +0300

    Add traffic light Arduino project
    Simulates a basic traffic light system using three LEDs with timed delays.

---
+++ traffic_light.ino
@@ -0,0 +1,30 @@
+int red = 12;
+int yellow = 11;
+int green = 10;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+
+void loop() {
+  // Green light ON
+  digitalWrite(green, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  delay(3000);
+
+  // Yellow light ON
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  digitalWrite(red, LOW);
+  delay(1000);
+
+  // Red light ON
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, LOW); 
+}
+  digitalWrite(red, HIGH);
+  delay(3000);
+}
